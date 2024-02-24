8e20d1c26c106f235be25fcb7962d2c0a5e8dd9e
Prateek Sunal <prtksunal@gmail.com>
2024-02-24 23:43:35 +0530

    fix: move database path to documents directory

--- lib/utils/directory_utils.dart
+++ lib/utils/directory_utils.dart
@@ -1,8 +1,12 @@
+import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
 class DirectoryUtils {
-  static Future<String> getDatabasePath(String databaseName) async =>
-      (await getDownloadsDirectory())!
-          .path
-          .replaceFirst('Downloads', '.$databaseName');
+  static Future<String> getDatabasePath(String databaseName) async => p.joinAll(
+        [
+          (await getApplicationDocumentsDirectory()).path,
+          "ente",
+          "auth.$databaseName",
+        ],
+      );
 }
